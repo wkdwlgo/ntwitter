894f5608d23cb8c7b72cfe8f47538f4fb7e09097
jisoleil <janggh1012@naver.com>
2024-06-20 17:12:41 +0900

    create-account styled component semi- complete

---
+++ src/firebase.tsx
@@ -0,0 +1,16 @@
+
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyD-3KUkE5uaMWlKpe2ObTqRRzOEi5x-m9E",
+  authDomain: "ntwitter-reloaded-8d95c.firebaseapp.com",
+  projectId: "ntwitter-reloaded-8d95c",
+  storageBucket: "ntwitter-reloaded-8d95c.appspot.com",
+  messagingSenderId: "19506617330",
+  appId: "1:19506617330:web:17346fb389d13be394e96b"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
