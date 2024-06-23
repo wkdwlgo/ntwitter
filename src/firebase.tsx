2d2c56c3ea4063843d0965a690e97253185f6368
jisoleil <janggh1012@naver.com>
2024-06-24 00:07:16 +0900

    post-tweet-form and firebase db connect

--- src/firebase.tsx
+++ src/firebase.tsx
@@ -1,6 +1,8 @@
 
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
   apiKey: "AIzaSyD-3KUkE5uaMWlKpe2ObTqRRzOEi5x-m9E",
@@ -14,3 +16,7 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
+
+export const storage=getStorage(app);
+
+export const db=getFirestore(app);
