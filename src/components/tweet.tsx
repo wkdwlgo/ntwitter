8e1f37a9df0746eeaae8500c210aef58725f962a
jisoleil <janggh1012@naver.com>
2024-06-24 19:14:51 +0900

    fetching timeline

---
+++ src/components/tweet.tsx
@@ -0,0 +1,40 @@
+import { ITweet } from "./timeline";
+import styled from "styled-components";
+const Wrapper=styled.div`
+    display: grid;
+    grid-template-columns: 3fr 1fr;
+    padding: 20px;
+    border:1px solid rgba(255,255,255,0.5);
+    border-radius: 15px;
+`;
+
+const Column=styled.div`
+
+`;
+
+const Photo=styled.img`
+    width: 100px;
+    height: 100px;
+    border-radius:15px;
+`;
+
+const Username= styled.span`
+    font-weight: 700;
+    font-size: 15px;
+`;
+
+const Payload=styled.p`
+    margin: 10px 0px;
+    font-size: 18px;
+`;
+export default function Tweet({username, photo, tweet}:ITweet){
+    return(
+        <Wrapper>
+            <Column>
+                <Username>{username}</Username>
+                <Payload>{tweet}</Payload>
+            </Column>
+            {photo ? <Column><Photo src={photo}/></Column>:null}
+        </Wrapper>
+    )
+}
--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,13 +1,19 @@
 import PostTweetForm from "../components/post-tweet-form";
 import styled from "styled-components";
+import Timeline from "../components/timeline";
 
-const Wrapper= styled.div``;
+const Wrapper= styled.div`
+    display: grid;
+    gap: 50px;
+    grid-template-rows: 1fr 5fr;
+`;
 
 export default function Home(){
     
     return (
        <Wrapper>
             <PostTweetForm/>
+            <Timeline/>
        </Wrapper>
     );
 }
