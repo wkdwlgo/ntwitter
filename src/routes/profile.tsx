6808fd4ace67310d36c6b32b527e131365482331
jisoleil <janggh1012@naver.com>
2024-06-26 23:24:13 +0900

    user's timeline

--- src/routes/profile.tsx
+++ src/routes/profile.tsx
@@ -1,8 +1,18 @@
-import styled from "styled-components";
-import { auth, storage } from "../firebase";
-import { useState } from "react";
+import { styled } from "styled-components";
+import { auth, db, storage } from "../firebase";
+import { useEffect, useState } from "react";
 import { getDownloadURL, ref, uploadBytes } from "firebase/storage";
 import { updateProfile } from "firebase/auth";
+import {
+  collection,
+  getDocs,
+  limit,
+  orderBy,
+  query,
+  where,
+} from "firebase/firestore";
+import { ITweet } from "../components/timeline";
+import Tweet from "../components/tweet";
 
 const Wrapper = styled.div`
     display: flex;
@@ -38,10 +48,18 @@ const Name=styled.span`
     font-size:22px;
 `;
 
+const Tweets=styled.div`
+    display: flex;
+    flex-direction:column;
+    gap:10px;
+    width:100%;
+`;
+
 export default function Profile(){
     const user=auth.currentUser;
     const FILE_SIZE_MAX_LIMIT=1*1024*1024;
     const [avatar, setAvatar]=useState(user?.photoURL);
+    const [tweets, setTweets]=useState<ITweet[]>([]);
     const onAvatarChange=async (e:React.ChangeEvent<HTMLInputElement>)=>{
         const {files} =e.target;
         if(files && files.length===1){
@@ -59,18 +77,58 @@ export default function Profile(){
             });
         }
     };
-    return (
-        <Wrapper>
-            <AvatarUpload htmlFor="avatar">
-                {Boolean(avatar)? <AvatarImg src={avatar}/>: <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6">
-  <path fillRule="evenodd" d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z" clipRule="evenodd" />
-</svg>
-}
-            </AvatarUpload>
-            <AvatarInput onChange={onAvatarChange} id="avatar" type="file" accept="image/*"/>
-            <Name>
-                {user?.displayName ?? "Anonymous"}
-            </Name>
-        </Wrapper>
+    const fetchTweets = async () => {
+    const tweetQuery = query(
+      collection(db, "tweets"),
+      where("userId", "==", user?.uid),
+      orderBy("createdAt", "desc"),
+      limit(25)
     );
+    const snapshot = await getDocs(tweetQuery);
+    const tweets = snapshot.docs.map((doc) => {
+      const { tweet, createdAt, userId, username, photo } = doc.data();
+      return {
+        tweet,
+        createdAt,
+        userId,
+        username,
+        photo,
+        id: doc.id,
+      };
+    });
+    setTweets(tweets);
+  };
+  useEffect(() => {
+    fetchTweets();
+  }, []);
+  return (
+    <Wrapper>
+      <AvatarUpload htmlFor="avatar">
+        {avatar ? (
+          <AvatarImg src={avatar} />
+        ) : (
+          <svg
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+            aria-hidden="true"
+          >
+            <path d="M10 8a3 3 0 100-6 3 3 0 000 6zM3.465 14.493a1.23 1.23 0 00.41 1.412A9.957 9.957 0 0010 18c2.31 0 4.438-.784 6.131-2.1.43-.333.604-.903.408-1.41a7.002 7.002 0 00-13.074.003z" />
+          </svg>
+        )}
+      </AvatarUpload>
+      <AvatarInput
+        onChange={onAvatarChange}
+        id="avatar"
+        type="file"
+        accept="image/*"
+      />
+      <Name>{user?.displayName ?? "Anonymous"}</Name>
+      <Tweets>
+        {tweets.map((tweet) => (
+          <Tweet key={tweet.id} {...tweet} />
+        ))}
+      </Tweets>
+    </Wrapper>
+  );
 }
